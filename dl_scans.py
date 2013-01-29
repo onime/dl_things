76d44ed722d57a1b072ac7a3aeb4c593c4a941c6
yosholo <mathieu.lgrand@gmail.com>
2013-01-29 17:33:14 +0100

    modifying some things

--- dl_scans.py
+++ dl_scans.py
@@ -4,7 +4,7 @@
 
 from lxml import etree
 from urllib.request import urlretrieve
-from easylast import infos_last_dl
+from easylast import *
 import socket
 import re
 import os
@@ -28,27 +28,26 @@ def get_links_scan(link_scan):
    
 def dl_images_of_a_scan(list_url,path_dirs):
 
-    count = 0
+    count = 1
     
     for u in list_url:
+
 #On parse chaque lien pour récupérer l'adresse de l'image
         tree_page_img = etree.parse(u,parserHTML)
         img = tree_page_img.xpath("//img[@id='p']")
 
-        count+=1
         link_img = img[0].attrib["src"]
-        
-        name_page = str(count)
-        if count < 10:
-            name_page = "0"+str(count)
 
-#On définit le bon path avec la bonne extension
-        path_file = path_dirs+"/"+name_page+os.path.splitext(link_img)[1]
+#On définit le bon path avec la bonne extension        
+        name_page = format_number_zero([count])[0] + os.path.splitext(link_img)[1]
+        path_file = path_dirs+"/"+ name_page
         
 #On la télécharge 
         urlretrieve(link_img,path_file)
         sys.stdout.write(name_page+" ")
         sys.stdout.flush()
+        count+=1
+
     print("")
 
 
@@ -57,6 +56,7 @@ file_rss = "http://feeds.feedburner.com/mstream"
 basename = "http://mangastream.com/"
 parserXML = etree.XMLParser(ns_clean=True, recover=True,encoding='utf-8')
 parserHTML = etree.HTMLParser()
+path_dirs = "/media/Data/test_scan/"
 
 #parse le fichier xml
 tree = etree.parse(file_rss,parserXML)
@@ -71,24 +71,23 @@ for i in items:
         search_scan = re.search("("+name_scan[0]+").*([0-9]{3})",title,re.IGNORECASE)
         if search_scan and int(search_scan.group(2)) > name_scan[1]:
             
-            real_name_scan = search_scan.group(1).replace(" ",".")
+            name_dir_scan = format_name(name_scan[0],' ')
             num_scan_dl = search_scan.group(2)            
             link_scan = childs[1].text
                 
-            path_dirs = "/media/Data/test_scan/"+real_name_scan+"/"+num_scan_dl
-            path_dirs = path_dirs.replace(" ",".")
+            path_dirs_scan = path_dirs+name_dir_scan
                 
-            if not os.path.exists(path_dirs):
-                os.makedirs(path_dirs)
+            if not os.path.exists(path_dirs_scan):
+                os.makedirs(path_dirs_scan)
 
 #On récupère les pages du scans                        
             list_link_scan = get_links_scan(link_scan)
 
-            sys.stdout.write(" Downlading "+real_name_scan+" "+num_scan_dl+" "+str(len(list_link_scan))+" pages ")
+            sys.stdout.write(" Downlading "+name_dir_scan+" "+num_scan_dl+" "+str(len(list_link_scan))+" pages ")
 #On télécharge les images des liens qu'on a récup
             dl_images_of_a_scan(list_link_scan,path_dirs)
             
             notify2.init("Scans Téléchargé")
-            notif = notify2.Notification(real_name_scan+" "+num_scan_dl+" "+str(len(list_link_scan))+" pages")
+            notif = notify2.Notification(name_dir_scan+" "+num_scan_dl+" "+str(len(list_link_scan))+" pages")
             notif.show()
             
