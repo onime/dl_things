df34c34ba6e850f85118c0b6640339fa171f2925
yosholo <mathieu.lgrand@gmail.com>
2013-01-29 16:30:11 +0100

    use module and fixing name of torrent

--- dl_scans.py
+++ dl_scans.py
@@ -4,39 +4,12 @@
 
 from lxml import etree
 from urllib.request import urlretrieve
+from easylast import infos_last_dl
 import socket
 import re
 import os
 import sys
 import notify2
-
-def get_list_scans():
-    port = 2345
-    host = "192.168.0.101"
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((host,port))
-    
-    sock.sendall(b"last_seen -d -w\n")
-    xml = sock.recv(4096).decode('utf-8')
-    
-    while re.search('\n',xml) == None:
-        xml +=sock.recv(4096).decode('utf-8')
-
-    sock.close()
-    print(xml)
-    xml = xml.replace('\n','')
-    root_xml = etree.fromstring(xml)
-    infos = root_xml.xpath("//info")
-    list_show = []
-
-    for i in infos:
-        print(i.attrib["name"])
-        i.attrib["name"] = i.attrib["name"].replace("."," ")
-        if i.attrib["type"] == "MANGA":
-            print(i.attrib["type"])
-            list_show.append([i.attrib["name"],i.attrib["num_chap"]])
-
-    return list_show
 
 def get_links_scan(link_scan):
 
@@ -52,32 +25,11 @@ def get_links_scan(link_scan):
         list_url.append(basename+childs[i].attrib["href"])
     
     return list_url
-
-def hook_retrieve(count_of_block,block_zise,total_size):
-#Pour la barre de progression
-    
-    if count_of_block > 0:
-        nb_bloc_total = int(total_size / block_zise)
-        nb_bloc_to_progress = int(nb_bloc_total / 50)
-        nb_bloc_to_draw = 1
-        nb_cara = 30
-
-        if nb_bloc_to_progress  <= 0:
-            nb_bloc_to_draw = int(nb_cara / nb_bloc_total)+1
-            nb_bloc_to_progress = 1
-
-        if count_of_block % nb_bloc_to_progress == 0:
-            if count_of_block < nb_cara / nb_bloc_to_draw:
-                sys.stdout.write("#"*nb_bloc_to_draw)
-                sys.stdout.flush()
-        if count_of_block == nb_bloc_total:
-                print("#"*int(1+(nb_cara / nb_bloc_total)))
-    
-    
-
+   
 def dl_images_of_a_scan(list_url,path_dirs):
 
     count = 0
+    
     for u in list_url:
 #On parse chaque lien pour récupérer l'adresse de l'image
         tree_page_img = etree.parse(u,parserHTML)
@@ -90,23 +42,21 @@ def dl_images_of_a_scan(list_url,path_dirs):
         if count < 10:
             name_page = "0"+str(count)
 
+#On définit le bon path avec la bonne extension
         path_file = path_dirs+"/"+name_page+os.path.splitext(link_img)[1]
         
-        sys.stdout.write("\t\t Downlading page "+ os.path.basename(link_img)+" ")
-        sys.stdout.flush()
-
 #On la télécharge 
-        urlretrieve(link_img,path_file,hook_retrieve)
+        urlretrieve(link_img,path_file)
+        sys.stdout.write(name_page+" ")
+        sys.stdout.flush()
+    print("")
 
-list = get_list_scans()
-print(list)
-exit(0)
+
+list_scan = infos_last_dl("MANGA"," ")
 file_rss = "http://feeds.feedburner.com/mstream"
 basename = "http://mangastream.com/"
 parserXML = etree.XMLParser(ns_clean=True, recover=True,encoding='utf-8')
 parserHTML = etree.HTMLParser()
-
-list_scan = ["Naruto","One Piece","Bleach","Fairy Tail","Claymore"]
 
 #parse le fichier xml
 tree = etree.parse(file_rss,parserXML)
@@ -118,27 +68,27 @@ for i in items:
 
 #Parmi les mangas présent on ne prend que ceux présent dans la liste
     for name_scan in list_scan:
-        search_scan = re.search(name_scan+".*([0-9]{3})",title,re.IGNORECASE)
-        if search_scan:
-            num_scan = search_scan.group(1)
-            link_scan = childs[1].text
+        search_scan = re.search("("+name_scan[0]+").*([0-9]{3})",title,re.IGNORECASE)
+        if search_scan and int(search_scan.group(2)) > name_scan[1]:
             
-            path_dirs = name_scan+" "+num_scan
-            path_dirs = path_dirs.replace(" ","-")
-
+            real_name_scan = search_scan.group(1).replace(" ",".")
+            num_scan_dl = search_scan.group(2)            
+            link_scan = childs[1].text
+                
+            path_dirs = "/media/Data/test_scan/"+real_name_scan+"/"+num_scan_dl
+            path_dirs = path_dirs.replace(" ",".")
+                
             if not os.path.exists(path_dirs):
-                  print("Creating "+path_dirs)                         
-                  os.makedirs(path_dirs)
+                os.makedirs(path_dirs)
 
-#On récupère les pages du scans            
-            print("\tGetting urls of "+name_scan)
+#On récupère les pages du scans                        
             list_link_scan = get_links_scan(link_scan)
-            print("\tPrepare Download "+name_scan+" Number pages : "+str(len(childs)-4))
 
+            sys.stdout.write(" Downlading "+real_name_scan+" "+num_scan_dl+" "+str(len(list_link_scan))+" pages ")
 #On télécharge les images des liens qu'on a récup
             dl_images_of_a_scan(list_link_scan,path_dirs)
             
             notify2.init("Scans Téléchargé")
-            notif = notify2.Notification(name_scan)
+            notif = notify2.Notification(real_name_scan+" "+num_scan_dl+" "+str(len(list_link_scan))+" pages")
             notif.show()
             
--- dl_torrent.py
+++ dl_torrent.py
@@ -9,40 +9,13 @@ import re
 import socket
 from urllib.request import urlopen
 import notify2
+from easylast import *
 
+path_dir = "/home/yosholo/.config/utils/torrent_file/"
+if not os.path.exists(path_dir):
+    os.makedirs(path_dir)
 
-def get_list_shows():
-    port = 2345
-    host = "192.168.0.101"
-    
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((host,port))
-    
-    sock.sendall(b"last_seen -d -w\n")
-    xml = sock.recv(4096).decode('utf-8')
-    
-    while re.search('\n',xml) == None:
-        xml +=sock.recv(4096).decode('utf-8')
-
-    sock.close()
-       
-    xml = xml.replace('\n','')
-    root_xml = etree.fromstring(xml)
-    infos = root_xml.xpath("//info")
-    list_show = []
-
-    for i in infos:
-        i.attrib["name"] = i.attrib["name"].replace("."," ")
-        if i.attrib["type"] == "SHOW":
-            list_show.append([i.attrib["name"],i.attrib["num_season"],i.attrib["num_episode"]])
-
-    return list_show
-   
-path = "/home/yosholo/.config/utils/torrent_file"
-if not os.path.exists(path):
-    os.makedirs(path)
-
-list_shows = get_list_shows()
+list_shows = infos_last_dl("SHOW"," ")
 
 file_rss = "http://eztv.it/showlist/"
 base_url = "http://eztv.it"
@@ -55,32 +28,36 @@ for i in items:
     for s in list_shows:
         if re.search(s[0],i.text,re.IGNORECASE):
             
+            name_dir_show = format_name(s[0],' ')
+
             #page de la liste des épisodes de la série
-            link_show = i.attrib["href"]
+            link_ep_show = i.attrib["href"]
             
-            tree_show = etree.parse(str(base_url+link_show),parserHTML)
-            name_episode = tree_show.xpath("//a[@class='epinfo']")
-            link_torrent = tree_show.xpath("//a[@class='download_2']")
+            tree_ep_show = etree.parse(str(base_url+link_ep_show),parserHTML)
+            names_episodes = tree_ep_show.xpath("//a[@class='epinfo']")
+            links_torrents = tree_ep_show.xpath("//a[@class='download_2']")
             
             #on parcour la liste des épisodes
             count = 0
-            for n in name_episode:
-                if re.search("(S[0-9]*"+s[1] + "E[0-9]*"+s[2]+")|("+s[1]+"x[0-9]*"+s[2]+")",n.text,re.IGNORECASE):
-                    if re.search("720p",n.text,re.IGNORECASE) == None:
-                        #On s'arrete quand on a trouvé un nom d'épisode avec le nom
-                        #de la dernière saison et épisode qui est téléchargé et qui ne contient pas 720p
-                        break
-                if re.search("720p",n.text,re.IGNORECASE) == None:
-                    
-                    #Si on s'est pas arreté on peut le télécharger et afficher une notif
-                    name = s[0]+"."+s[1]+"x"+s[2]+".torrent"
-                    path += name
-                    
-                    urlretrieve(link_torrent[count].attrib["href"],path)
 
-                    notify2.init("Torrent Téléchargé")
-                    notif = notify2.Notification(name)
-                    notif.show()
+            for n in names_episodes:
 
+                m = re.search(re_nseason_nep,n.text,re.IGNORECASE) 
+                if m != None:
+                    (num_season_cur,num_episode_cur) = parse_regex(m)
+
+                    if int(num_episode_cur) > s[3] and re.search("720p",n.text,re.IGNORECASE) == None:
+                        
+                        name_file = name_dir_show+".S"+num_season_cur+"E"+num_episode_cur+".torrent"
+                        path_torrent = path_dir+name_file
+
+                        urlretrieve(links_torrents[count].attrib["href"],path_torrent)
+                        notify2.init("Torrent Téléchargé")
+                        notif = notify2.Notification(name_file)
+                        notif.show()
+                        
+                    elif int(num_episode_cur) <= s[3]:
+                        break;
+           
                 count+=1
           
