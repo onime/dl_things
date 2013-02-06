73834c8eb5da8d75b1ce4b526bca1158ea7d727e
yosholo <mathieu.lgrand@gmail.com>
2013-02-06 20:48:30 +0100

    fixing bug whit sort

--- dl_sub.py
+++ dl_sub.py
@@ -22,14 +22,14 @@ for dir_show in dirs_show:
     if os.path.isdir(dir_show):
         for show in list_shows:
             if re.search(show[0].replace(" ","."),dir_show,re.IGNORECASE):
-                list_srt = sorted(glob.glob(dir_show+"/Saison."+str(show[1])+"/*.srt"))
+                list_srt = sorted([x.lower() for x in glob.glob(dir_show+"/Saison."+str(show[1])+"/*.srt")])
                 
                 str_ep_sub = re.match(".*S[0-9]+E([0-9]+)\.",list_srt[-1],re.IGNORECASE).group(1)
                 
                 last_sub.append([show[0],show[1],int(str_ep_sub),dir_show])
 
-
-
+print(last_sub)
+exit(0)
 base_addicted = "http://www.addic7ed.com/"
 tree_addicted = etree.parse(base_addicted+"shows.php",parserHTML)
 
