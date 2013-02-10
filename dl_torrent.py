ed3b2e196ed4bea6dcb473d1044466ad2e154d61
yosholo <mathieu.lgrand@gmail.com>
2013-02-10 16:55:51 +0100

    use client_last in dl_torrent

--- dl_torrent.py
+++ dl_torrent.py
@@ -15,7 +15,7 @@ path_dir = "/home/yosholo/.config/utils/torrent_file/"
 if not os.path.exists(path_dir):
     os.makedirs(path_dir)
 
-list_shows = infos_last_dl("SHOW"," ")
+list_shows = infos_last("SHOW"," ","DL")
 
 file_rss = "http://eztv.it/showlist/"
 base_url = "http://eztv.it"
@@ -42,25 +42,25 @@ for i in items:
 
             for n in names_episodes:
 
-                m = re.search(re_nseason_nep,n.text,re.IGNORECASE) 
+                m = re.search(regex_infos,n.text,re.IGNORECASE) 
                 if m != None:
 
                     (num_season_cur,num_episode_cur) = parse_regex(m)
                     
-                    if  (int(num_season_cur) > s[1] or
-                    (int(num_season_cur) == s[1] and int(num_episode_cur) > s[2]) and
+                    if  (int(num_season_cur) > int(s[1]) or
+                    (int(num_season_cur) == int(s[1]) and int(num_episode_cur) > int(s[2])) and
                     (re.search("720p",n.text,re.IGNORECASE)) == None):
                         
                         name_file = name_dir_show+".S"+num_season_cur+"E"+num_episode_cur+".torrent"
                         path_torrent = path_dir+name_file
-
+                        
                         urlretrieve(links_torrents[count].attrib["href"],path_torrent)
                        
                         notify2.init("Torrent Téléchargé")
                         notif = notify2.Notification(name_file)
                         notif.show()
                         
-                    elif int(num_season_cur) < s[1] or (int(num_season_cur) == s[1] and int(num_episode_cur)) <= s[2]:
+                    elif int(num_season_cur) < int(s[1]) or (int(num_season_cur) == int(s[1]) and int(num_episode_cur)) <= int(s[2]):
                         break;
            
                 count+=1
