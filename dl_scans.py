cd78337bf1bbf8af35eb49c4355988e106ef6751
yosholo <mathieu.lgrand@gmail.com>
2013-08-10 00:32:27 +0200

    change the XPATH because mangastream had a new version

--- dl_scans.py
+++ dl_scans.py
@@ -3,7 +3,7 @@
 #Télécharge les scans de mangastream
 
 from lxml import etree
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve,urlopen,Request
 from easylast import *
 import re
 import os
@@ -11,41 +11,39 @@ import sys
 import notify2
 import subprocess
 
+
+
 def get_links_scan(link_scan):
 
 #On parse une page d'un scan pour récupérer les liens de toutes les pages du scan
     tree_page_princ = parse_url(link_scan)
-    id_controls = tree_page_princ.xpath("//div[@id='controls']")
-    
-    childs= id_controls[0].getchildren()
-    
-    list_url = []
-#On évite les deux premiers liens et les deux derniers car ce sont les liens prev next une balise div et une strong RAF
-    for i in range(2,len(childs)-2):
-        list_url.append(basename+childs[i].attrib["href"])
+    list_url = tree_page_princ.xpath("//div[@class='btn-group'][2]/ul[@class='dropdown-menu']/li/a/@href")
     
     return list_url
    
 def dl_images_of_a_scan(list_url,path_dirs):
 
     count = 1
-    
     for u in list_url:
-
-#On parse chaque lien pour récupérer l'adresse de l'image
+        
+        #On parse chaque lien pour récupérer l'adresse de l'image
         tree_page_img = parse_url(u)
-        img = tree_page_img.xpath("//img[@id='p']")
+        link_img = tree_page_img.xpath("//img[@id='manga-page']/@src")[0]
 
-        link_img = img[0].attrib["src"]
-        
-        #On définit le bon path avec la bonne extension    
-        name_page = str(format_number_zero({"page":int(count)})["page"]) + os.path.splitext(link_img)[1]
-        
+        #On définit le bon path avec la bonne extension
+        name_page = link_img.split('/')[-1]
+
         path_file = path_dirs+"/"+ name_page
-                
+        
         #On la télécharge 
-        urlretrieve(link_img,path_file)
+        content_img = urlopen(Request(link_img,headers={'User-Agent': 'Mozilla/5.0'})).read().decode("iso-8859-1")
+        file_img = open(path_file,"w",encoding = "iso-8859-1")
+        file_img.write(content_img)
+        file_img.close()
+               
+#        urlretrieve(link_img,path_file)
         count+=1
+
 
 def dl_page_one_manga(name,num,page):
     
@@ -122,27 +120,28 @@ else:
         childs = i.getchildren()  
         title = childs[0].text
 
-    #Parmi les mangas présent on ne prend que ceux présent dans la liste
-    for info_scan in list_scan:
-        if re.search("("+info_scan["name"]+")",title,re.IGNORECASE):
-            num_scan_dl = parse_regex(re.search(regex_infos,title,re.IGNORECASE))["chap"]
-        
-            if int(num_scan_dl) > int(info_scan["num"]["chap"]):
-                
-                name_dir_scan = format_name(info_scan["name"],' ')
-                link_scan = childs[1].text
-                
-                path_dirs_scan = path_dirs+name_dir_scan+"/"+num_scan_dl+"/"
+        #Parmi les mangas présent on ne prend que ceux présent dans la liste
+        for info_scan in list_scan:
 
-                if name_dir_scan not in hash_scan.keys():
-                    hash_scan[name_dir_scan] = []
+            if re.search("("+info_scan["name"]+")",title,re.IGNORECASE):
+                num_scan_dl = parse_regex(re.search(regex_infos,title,re.IGNORECASE))["chap"]
 
-                hash_scan[name_dir_scan].append(num_scan_dl)
+                if int(num_scan_dl) > int(info_scan["num"]["chap"]):
+                
+                    name_dir_scan = format_name(info_scan["name"],' ')
+                    link_scan = childs[1].text
+                
+                    path_dirs_scan = path_dirs+name_dir_scan+"/"+num_scan_dl+"/"
+
+                    if name_dir_scan not in hash_scan.keys():
+                        hash_scan[name_dir_scan] = []
+
+                        hash_scan[name_dir_scan].append(num_scan_dl)
                             
-                if not os.path.exists(path_dirs_scan):
-                    os.makedirs(path_dirs_scan)
-
-#On récupère les pages du scans                        
+                    if not os.path.exists(path_dirs_scan):
+                        os.makedirs(path_dirs_scan)
+                    print(link_scan)
+                #On récupère les pages du scans
                 list_link_scan = get_links_scan(link_scan)
 
                 print(" Downlading "+name_dir_scan+" "+num_scan_dl+" "+str(len(list_link_scan))+" pages ")
