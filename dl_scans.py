8aef657b1620d3898ebfc40d3469b6903dbc7b34
yosholo <mathieu.lgrand@gmail.com>
2013-08-16 23:07:42 +0200

    add param --list-new and modification of architecture

--- dl_scans.py
+++ dl_scans.py
@@ -2,6 +2,7 @@
 
 #Télécharge les scans de mangastream
 
+from operator import attrgetter
 from lxml import etree
 from urllib.request import urlretrieve,urlopen,Request
 from easylast import *
@@ -74,87 +75,105 @@ def dl_one_manga(name,num):
         print("\t\t",name,num,"no available")
 
     #there is two select one at the top of the image and one at the bottom but i need only one
-    
+
+def dl_missing_scans():
+
+    name_one_manga={"naruto":"naruto_manga","bleach":"bleach","claymore":"claymore","fairy tail":"Fairy_Tail","one piece":"One_Piece"}
+    fail_load = []
    
-        
-name_one_manga={"naruto":"naruto_manga","bleach":"bleach","claymore":"claymore","fairy tail":"Fairy_Tail","one piece":"One_Piece"}
-fail_load = []
+
+    for scan in list_scan:
+            
+        str_num_chaps = os.listdir("/media/Data/Scans/"+format_name(scan['name']," "))
+        num_chaps = [int(n) for n in str_num_chaps if n.isdigit()]
+        num_chaps = sorted(num_chaps)
+            
+    count = 1
+    print(name_one_manga[scan['name']])
+
+    for n in num_chaps:
+        if count != n:
+            print("\tDownload",count,n-1)
+            for i in range(count,n):
+                dl_one_manga(scan['name'],i)
+                
+                count = n
+            count+=1
+
+        print(fail_load)
+
+def get_list_new_chap():
+    
+    file_rss = "http://feeds.feedburner.com/mstream"
+    #parse the xml rss
+    tree = parse_url(file_rss,"xml")
+    names = tree.xpath('//item/title/text()')
+    links = tree.xpath('//item/link/text()')
+
+    list_scan_out = []
+
+    #go through the names and get the links and num of the last scans
+    for (i,name) in enumerate(names):
+        for info_scan in list_scan:
+            if re.search("("+info_scan["name"]+")",name,re.IGNORECASE):
+                
+                info_chap_curr = {}
+                info_chap_curr["num"] = name.split(" ")[-1]
+                info_chap_curr["name"] = info_scan["name"]
+                info_chap_curr["link"] = links[i]
+
+                #We keep the most recent
+                if info_chap_curr["num"].isdigit():
+                    if int(info_chap_curr["num"]) > info_scan["num"]["chap"]:
+                        list_scan_out.append(info_chap_curr)
+                else:
+                    list_scan_out.append(info_chap_curr)
+
+    #sort the list by the name then by the num 
+    return sorted(list_scan_out,key=lambda k: (k["name"],k["num"]))
+
+def print_new_chap(list_chap):
+
+    for chap in list_chap:
+        print(format_name(chap["name"]," "),chap["num"],chap["link"])
 
 list_scan = infos_last("MANGA"," ","DL")
 
 if len(sys.argv) > 1:
-    if(sys.argv[1] == "--missing"):
+    if sys.argv[1] == "--list-new":    
+        print_new_chap(get_list_new_chap())
+    exit(0)
+
+
+basename = "http://mangastream.com/"
+path_dirs = "/media/Data/Scans/"
+
+dl_missing_scans()
+     
+list_scan_out = get_list_new_chap()
+    
+#loop in the latest scan and dl it
+for item in list_scan_out:
         
-        for scan in list_scan:
+    name_dir_scan = format_name(item["name"],' ')
+    path_dirs_scan = path_dirs+name_dir_scan+"/"+item["num"]+"/"
+
+    #get a list of the pages
+    list_link_scan = get_links_scan(item["link"])
+        
+    if len(list_link_scan) > 0:
             
-            str_num_chaps = os.listdir("/media/Data/Scans/"+format_name(scan['name']," "))
-            num_chaps = [int(n) for n in str_num_chaps if n.isdigit()]
-            num_chaps = sorted(num_chaps)
-            
-            count = 1
-            print(name_one_manga[scan['name']])
+        if not os.path.exists(path_dirs_scan):
+            print("Creating Directory",path_dirs_scan)
+            os.makedirs(path_dirs_scan)
 
-            for n in num_chaps:
-                if count != n:
-                    print("\tDownload",count,n-1)
-                    for i in range(count,n):
-                        dl_one_manga(scan['name'],i)
-                        
-                    count = n
-                count+=1
+        print(" Downlading "+name_dir_scan+" "+item["num"]+" "+str(len(list_link_scan))+" pages ")
 
-        print(fail_load)
-else:
-
-    file_rss = "http://feeds.feedburner.com/mstream"
-    basename = "http://mangastream.com/"
-    
-    path_dirs = "/media/Data/Scans/"
-    
-    #parse le fichier xml
-    tree = parse_url(file_rss,"xml")
-    items = tree.xpath('//item')
-    hash_scan = {} 
-
-    for i in items:
-        childs = i.getchildren()  
-        title = childs[0].text
-
-        #Parmi les mangas présent on ne prend que ceux présent dans la liste
-        for info_scan in list_scan:
-
-            if re.search("("+info_scan["name"]+")",title,re.IGNORECASE):
-                num_scan_dl = parse_regex(re.search(regex_infos,title,re.IGNORECASE))["chap"]
-
-                if int(num_scan_dl) > int(info_scan["num"]["chap"]):
-                
-                    name_dir_scan = format_name(info_scan["name"],' ')
-                    link_scan = childs[1].text
-                
-                    path_dirs_scan = path_dirs+name_dir_scan+"/"+num_scan_dl+"/"
-
-                    if name_dir_scan not in hash_scan.keys():
-                        hash_scan[name_dir_scan] = []
-
-                        hash_scan[name_dir_scan].append(num_scan_dl)
-                            
-                    if not os.path.exists(path_dirs_scan):
-                        os.makedirs(path_dirs_scan)
-                    print(link_scan)
-                #On récupère les pages du scans
-                list_link_scan = get_links_scan(link_scan)
-
-                print(" Downlading "+name_dir_scan+" "+num_scan_dl+" "+str(len(list_link_scan))+" pages ")
-#On télécharge les images des liens qu'on a récup
-                dl_images_of_a_scan(list_link_scan,path_dirs_scan)
-                
-                notify2.init("Scans Téléchargé")
-                notif = notify2.Notification(name_dir_scan+" "+num_scan_dl+" "+str(len(list_link_scan))+" pages")
-                notif.show()
-                
-    for k in hash_scan:
-        hash_scan[k] = sorted(hash_scan[k])
-        print("maj")
-        for num in hash_scan[k]:
-            upd_last(k,{"chap":num},"DL")
-
+        dl_images_of_a_scan(list_link_scan,path_dirs_scan)
+        
+        notify2.init("Scans Téléchargé")
+        notif = notify2.Notification(name_dir_scan+" "+item["num"]+" "+str(len(list_link_scan))+" pages")
+        notif.show()
+        
+        if item["num"].isdigit():
+            upd_last(name_dir_scan,{"chap":item["num"]},"DL")
